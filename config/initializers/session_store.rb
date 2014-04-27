b930f18e06a99b1d4bb8955ef1cd04d42b6a9e5c
eritiro <eritiro@gmail.com>
2014-04-27 16:32:42 -0300

    initial

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TellServer::Application.config.session_store :cookie_store, key: '_tell-server_session'
---
+++ db/migrate/20140427191359_create_locations.rb
@@ -0,0 +1,11 @@
+class CreateLocations < ActiveRecord::Migration
+  def change
+    create_table :locations do |t|
+      t.string :name
+      t.string :address
+      t.string :phone
+
+      t.timestamps
+    end
+  end
+end
