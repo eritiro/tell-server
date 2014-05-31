6515a5b63c338baa9bd1868cd91821e6b2633061
eritiro <eritiro@gmail.com>
2014-05-31 15:47:51 -0300

    default images

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_attached_file :picture, :styles => { :medium => "400x400>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :picture, :styles => { :medium => "400x400>", :thumb => "100x100>" }, :default_url => "/assets/missing_:style.png"
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 
   def to_s
