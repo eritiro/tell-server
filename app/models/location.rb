e47c7fa5badebfa864ee5ea01be93f537f97a474
eritiro <eritiro@gmail.com>
2014-08-26 23:49:30 -0300

    test fix

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,6 +2,7 @@ class Location < ActiveRecord::Base
   has_many :comments
   has_attached_file :photo, styles: { medium: "400>x200", thumb: "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  validates :afip_req, uniqueness: true
 
   def score
     return nil if comments.empty?
