3ef736519f70b40c5c9bd13c0181d414e66cd1eb
eritiro <eritiro@gmail.com>
2014-09-05 21:50:51 -0300

    patch some afip urls that were wrong

--- app/crawlers/location_crawler.rb
+++ app/crawlers/location_crawler.rb
@@ -1,5 +1,6 @@
 class LocationCrawler
   require 'open-uri'
+  require 'open_uri_redirections'
   require 'nokogiri'
 
   class LocationCrawlerError < StandardError
@@ -9,7 +10,7 @@ class LocationCrawler
     validate_url afip_url
     location = Location.new(afip_url: afip_url)
     OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:ssl_version] = 'SSLv3'
-    unparsed = open(afip_url)
+    unparsed = open(afip_url, :allow_redirections => :safe)
     doc = Nokogiri::HTML(unparsed)
     if doc.css('#lblMensaje').present?
       raise LocationCrawlerError.new("AFIP Message: #{doc.css('#lblMensaje').text}")
