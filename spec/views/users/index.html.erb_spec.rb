9d2e47caefe86df9c57a76a13d61d06d593afdc5
eritiro <eritiro@gmail.com>
2014-09-11 12:01:37 -0300

    foo

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -2,22 +2,23 @@ require 'spec_helper'
 
 describe "users/index" do
   before(:each) do
-    assign(:users, [
-      stub_model(User,
-        :username => "Username",
-        :email => "Email"
-      ),
-      stub_model(User,
-        :username => "Username",
-        :email => "Email"
-      )
-    ])
+    @identities = []
+    @identities << build(:identity, provider: 'facebook')
+    @identities << build(:identity, provider: 'twitter')
+
+    @users = assign(:users, build_list(:user, 2, identities: @identities))
   end
 
   it "renders a list of users" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Username".to_s, :count => 2
-    assert_select "tr>td", :text => "Email".to_s, :count => 2
+    assert_select "tr>td", text: @users.first.username.to_s
+    assert_select "tr>td", text: @users.last.username.to_s
+    assert_select "tr>td", text: @users.first.email.to_s
+    assert_select "tr>td", text: @users.last.email.to_s
+  end
+
+  it "renders the social networks providers" do
+    render
+    assert_select "tr>td", text: "facebook, twitter"
   end
 end
