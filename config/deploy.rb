de658e08ee3410214a366b55ab79eab4cba515ef
eritiro <eritiro@gmail.com>
2014-12-30 21:22:29 -0300

    deploy updated

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,15 +4,13 @@ require "rvm/capistrano"  # Load RVM's capistrano plugin.
 set :rvm_type, :system
 set :rvm_path, "/usr/local/rvm"
 set :rvm_ruby_string, "ruby-2.1.2"
-#set :rvm_custom_path, "/usr/local/rvm/bin/"
 set :application, "tell-server"
 
-set :repository,  "https://github.com/Pomeeelo/tell-server.git"
+set :repository, "git@bitbucket.org:eritiro/tell-server.git"
 set :branch, "master"
 set :scm, "git"
 set :user, "deployer"
 set :deploy_via, :remote_cache
-#set :copy_remote_dir, "/tmp/tell-server"
 
 server "tell.startmeapps.com", :app, :web, :db, :primary => true
 set :deploy_to, '/deployment/tell-server'
@@ -26,9 +24,6 @@ before "deploy", "deploy:setup"
 
 #always migrates the database
 after 'deploy:update_code', 'deploy:migrate'
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
 # namespace :deploy do
